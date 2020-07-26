abc064f46d6daca78647fa341c309b6a4affac5b
Takahiro <tkhr_621@docomo.ne.jp>
2020-07-26 19:41:29 +0900

    add preview.js

---
+++ .storybook/preview.js
@@ -0,0 +1,9 @@
+// viewportの設定
+import { addParameters } from '@storybook/react'
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport'
+
+addParameters({
+  viewport: {
+    viewports: INITIAL_VIEWPORTS,
+  },
+})
