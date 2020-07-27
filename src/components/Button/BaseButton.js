c9d4bc1eac21c6f2cc2d2daa457a569f0f1e60c3
Takahiro <tkhr_621@docomo.ne.jp>
2020-07-27 23:46:13 +0900

    basebutton第一弾

--- src/components/Button/BaseButton.js
+++ src/components/Button/BaseButton.js
@@ -3,10 +3,22 @@ import React from 'react'
 import { jsx, css } from '@emotion/core'
 
 const style = css`
-  background-color: #444;
-  &:hover {
-    color: #000;
-  }
+display: inline-flex;
+justify-content: center;
+align-items: center;
+width: 100%;
+height: 40px;
+min-width: 2rem;
+vertical-align: middle;
+border-radius: 1em;
+font-family: inherit;
+font-weight: bold;
+text-align: center;
+text-decoration: none;
+box-sizing: border-box;
+cursor: pointer;
+transition: none;
+white-space: nowrap;
 `
 
 // export const BaseButton = ({ onClick, text }) => (
