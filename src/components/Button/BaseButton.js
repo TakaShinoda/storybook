9de7bc78bab46c585a81a64664fbe1cccea68bd6
Takahiro <tkhr_621@docomo.ne.jp>
2020-07-27 23:13:34 +0900

    お試しスタイル

--- src/components/Button/BaseButton.js
+++ src/components/Button/BaseButton.js
@@ -1,5 +1,18 @@
+/** @jsx jsx */
 import React from 'react'
+import { jsx, css } from '@emotion/core'
 
-export const BaseButton = ({ onClick, text }) => (
-  <button onClick={onClick}>{text}</button>
+const style = css`
+  background-color: #444;
+  &:hover {
+    color: #000;
+  }
+`
+
+// export const BaseButton = ({ onClick, text }) => (
+//   <button onClick={onClick}>{text}</button>
+// )
+
+export const BaseButton = ({ onClick }) => (
+  <button onClick={onClick} css={style}>Button</button>
 )
